6fb3a339faee323c3c4f57e07609ebbaa277a9ff
Mike <mikevanverseveld@hotmail.com>
2023-11-16 20:52:45 +0100

    Progress ziglings
    Progress ziglings

--- exercises/049_quiz6.zig
+++ exercises/049_quiz6.zig
@@ -27,7 +27,16 @@ const Elephant = struct {
     // Your Elephant trunk methods go here!
     // ---------------------------------------------------
 
-    ???
+    pub fn getTrunk(self: *Elephant) *Elephant {
+        return self.trunk.?;
+    }
+
+    pub fn hasTrunk(self: *Elephant) bool {
+        if (self.trunk == null) {
+            return false;
+        }
+        return true;
+    }
 
     // ---------------------------------------------------
 
